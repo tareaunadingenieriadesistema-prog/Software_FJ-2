e37f07fa9ae01aa3e56ece3e297da20ea6b9676d
Jesus_Salcedo-35 <tareaunadingenieriadesistema@gmail.com>
2026-05-11 19:12:47 -0500

    Remove fifth student's name from comments
    Removed the name of the fifth student from the comments.

--- cliente.py
+++ cliente.py
@@ -2,7 +2,6 @@
 # Nombre de estudiante2: Angela Patricia Perez Corpas
 # Nombre de estudiante3: Cristian Jose Ortega Paternina
 # Nombre de estudiante4: Jesus Andres Salcedo Martinez
-# Nombre de estudiante5: Rodrigo Alberto Gaviria Arroyo
 # Grupo: 213023_80
 # Programa: Ingenieria de sistemas
 # Código Fuente: autoría propia
